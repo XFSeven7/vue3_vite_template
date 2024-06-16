c3e1dde67fef56173cdef99bcb06a2921512e419
kuafu <kuafu@u1s1.biz>
2024-06-16 16:48:46 +0400

    添加全局样式

--- src/api/home.ts
+++ src/api/home.ts
@@ -3,6 +3,6 @@ import {request} from "@/api/core/request";
 /**
  * 模拟 获取数据 接口
  */
-export function login(params: any): Promise<any> {
+export function getData(params: any): Promise<any> {
     return request.get("/xxx/xxx/getData", params);
 }
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,3 +3,29 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script setup lang="ts">
+
+import {onMounted} from "vue";
+import {getData} from "@/api/home";
+
+
+onMounted(() => {
+
+  // 演示： 模拟数据请求
+  getData({
+    page: 1,
+    size: 20
+  }).then(data => {
+  }).catch(error => {
+  }).finally(() => {
+  });
+
+})
+
+</script>
+
+<style scoped>
+
+
+</style>
